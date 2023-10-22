77709767be044da224fde8d1950b35de6a9d8613
Enzo Weder <enzowederma@gmail.com>
2023-10-22 16:41:17 -0300

    ✨ feat: add manual ID for ContaBancaria

--- src/main/java/org/example/entities/ContaBancaria.java
+++ src/main/java/org/example/entities/ContaBancaria.java
@@ -1,15 +1,12 @@
 package org.example.entities;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToOne;
 
 @Entity
 public class ContaBancaria {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     int idConta;
     
     @OneToOne
@@ -17,7 +14,8 @@ public class ContaBancaria {
     
     double saldo;
 
-    public ContaBancaria(Cliente titular, double saldo) {
+    public ContaBancaria(int idConta, Cliente titular, double saldo) {
+        this.idConta = idConta;
         this.titular = titular;
         this.saldo = saldo;
     };
