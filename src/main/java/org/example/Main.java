94936e7c3c7b740b761ede71f6128301f9fac30b
Enzo Weder <enzowederma@gmail.com>
2023-10-22 16:40:01 -0300

    🎈 perf: clear Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,19 +1,11 @@
 package org.example;
 
 import jakarta.persistence.EntityManager;
-import jakarta.persistence.EntityManagerFactory;
-import org.example.persistence.CustomPersistenceUnitInfo;
-import org.hibernate.jpa.HibernatePersistenceProvider;
-
-
-import java.util.HashMap;
-import java.util.Map;
 
 public class Main {
 
     public static void main(String[] args) {
         EntityManager em = HibernateManager.em;
-
 
         try {
             em.getTransaction().begin();
@@ -24,5 +16,4 @@ public class Main {
             em.close();
         }
     }
-
 }
