0d674f4dc04a60d26b59c5c1b98959dee6e76557
Enzo Weder <enzowederma@gmail.com>
2023-10-22 19:50:31 -0300

    🐞 fix: add examples into Main.class

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,5 +5,8 @@ public class Main {
     public static void main(String[] args) {
         //sExampleManager.populateViaHibernate();
         //ExampleManager.busqueContaBancaria(1);
+        //ExampleManager.deletaContaBancaria(1);
+        //ExampleManager.zeraSaldoContaBancaria(1);
     }
+    
 }
