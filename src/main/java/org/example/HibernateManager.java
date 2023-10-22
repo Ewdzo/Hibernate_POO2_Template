8af450fb01438c8f87e8ec84209fb75f4725a01e
Enzo Weder <enzowederma@gmail.com>
2023-10-22 16:38:59 -0300

    🐞 fix: change HibernateManager initialization

--- src/main/java/org/example/HibernateManager.java
+++ src/main/java/org/example/HibernateManager.java
@@ -12,12 +12,15 @@ import jakarta.persistence.EntityManagerFactory;
 public class HibernateManager {
     static String puName = "pu-name";
     static Map<String, String> props = new HashMap<>();
-    static EntityManagerFactory emf = new HibernatePersistenceProvider().createContainerEntityManagerFactory(new CustomPersistenceUnitInfo(puName), props);
-    static EntityManager em = emf.createEntityManager();
+    static EntityManagerFactory emf;
+    static EntityManager em;
 
     static {
         props.put("hibernate.show_sql", "true");
         props.put("hibernate.hbm2ddl.auto", "create");
+
+        emf = new HibernatePersistenceProvider().createContainerEntityManagerFactory(new CustomPersistenceUnitInfo(puName), props);
+        em = emf.createEntityManager();
     }
 
 
